e4317c06b4233dea546f2e45765017f0bc6cd999
Arthur <arthur@thewandb.com>
2024-05-03 12:09:44 +0100

    break out donation script into external handler

---
+++ src/fatalcenturion/donate_4_me.ts
@@ -0,0 +1,98 @@
+/**
+ * @author altrius
+ * @description A utility script to automate and standardise the handling of donations
+ * CAUTION: EARLY ACCESS
+ * LEVEL: MIDSEC
+ */
+export default (context: Context, args: { donate?: string | number, source?: boolean }) => {
+    // If the user wants to see the source...
+    if (args.source) return $fs.scripts.quine();
+    const caller = context.calling_script;
+
+    // Preload the stdlib
+    const l = $fs.scripts.lib()
+    // if the user wishes to donate, this is where we process that information
+    if (args.donate) {
+        let balance = $hs.accts.balance({ is_script: true })
+        let donation = -1;
+
+        // if the donation was provided as a string, convert to number, or do nothing
+        if (typeof args.donate !== "number") {
+            let donation_num = l.to_gc_num(args.donate);
+            if (typeof donation_num !== "number") {
+                l.log(``)
+                l.log(`\t\`DDonation Failed - Invalid Amount\``)
+                l.log(`\t\`8Your donation amount could not be converted into a an acceptable value\``)
+                l.log(`\t\`4We apologise for any inconvenience caused.\``)
+            } else {
+                donation = donation_num;
+            }
+        } else donation = args.donate
+
+        // validate that the donation is not negative, 0, or higher than the caller's balance
+        if (donation < 0) {
+            l.log(``)
+            l.log(`\t\`DDonation Failed - Invalid Amount\``)
+            l.log(`\t\`8The donation must be > 0\``)
+            l.log(`\t\`4We apologise for any inconvenience caused.\``)
+        } else if (donation > balance) {
+            l.log(``)
+            l.log(`\t\`DDonation Failed - Insufficient Funds\``)
+            l.log(`\t\`8The donation must be < ${l.to_gc_str(balance)}\``)
+            l.log(`\t\`4We apologise for any inconvenience caused.\``)
+        } else {
+            // calculate a value to send to the donation cause based on a percentage
+            let tax = Math.round((100 / donation) * 5);
+            let final = donation - tax;
+
+            let dono_res = $ns.accts.xfer_gc_to({
+                to: "fatalcenturion",
+                amount: final,
+                memo: "Donation Through altrius.finds_things, the free finder"
+            })
+            if (!dono_res.ok) {
+                l.log(``)
+                l.log(`\`DDonation Failed - Unknown Error\``)
+                l.log(`\`8We're not sure what happened, no GC was taken from your account\``)
+                l.log(`\`4We apologise for any inconvenience caused.\``)
+                return { ok: true, msg: l.get_log().join("\n") }
+            }
+
+
+            let tax_res = $ms.accts.xfer_gc_to({
+                to: "fatalcenturion", // for testing
+                //to: "wiz", // send GC to the correct wizmud trustee
+                amount: tax,
+                memo: "Donation Through altrius.finds_things, the free finder"
+            })
+
+            if (!tax_res.ok) {
+                l.log(`\`DDonation Failed - Unknown Error\``)
+                l.log(`\`8We're not sure what happened, we have fully refunded any GC taken from your account.\``)
+                l.log(`\`4We apologise for any inconvenience caused.\``)
+                $fs.accts.xfer_gc_to_caller({
+                    amount: final,
+                    memo: "Your donation was refunded due to a discrepancy."
+                })
+                return { ok: true, msg: l.get_log().join("\n") }
+            }
+            l.log(``)
+            l.log(`\`2Donation Successful\``)
+            l.log(`\`8- To creator: ${l.to_gc_str(final)}\``)
+            l.log(`\`8- To wizMUD:  ${l.to_gc_str(tax)} (5%)\``)
+            l.log(`\`2Thank you for your donation!\``)
+        }
+    } else {
+        l.log(``)
+        l.log(`\`6Want to support my work? Feeling generous?\``)
+        l.log(`\`6Use ${context.calling_script} {donate:<amount>} to thank me!\``)
+        l.log(`\`4A percentage of every donation is forwarded to wizMUD\``)
+        l.log(`\`4You will receive a statement detailing your donation\``)
+        l.log(`\`4once the transaction is completed\``)
+        l.log(`\`ECAUTION: Donations are handled by a MIDSEC script, called internally\``)
+        l.log(`\`EYou may view the source code of this script by passing the argument {donationSource: true}\``)
+        l.log(`\`EIt can also be viewed at \`\`3https:\/\/github\.com\/altriusrs\/hackmud\/blob\/main\/src\/fatalcenturion\/donate_4_me.ts\``)
+    }
+
+    return { ok: true, msg: l.get_log().join("\n").replaceAll('"', '') }
+}
