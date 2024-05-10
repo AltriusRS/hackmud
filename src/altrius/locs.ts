fa2f9d788d3d424e422a9141d28c195f885d4261
Arthur Amos <tom.b.2k2@gmail.com>
2024-05-10 04:12:19 +0100

    make personal loc utility for t2 using ast's scraper

--- src/altrius/locs.ts
+++ src/altrius/locs.ts
@@ -5,59 +5,101 @@
  * @level FULLSEC
  */
 type Scriptor = { name: string, call: (args: any) => unknown }
-export default (context: Context, args: { t: Scriptor, confirm?: boolean }) => {
+export default (context: Context, args: { t: Scriptor, confirm?: boolean, rank: string, class: string }) => {
   const l = $fs.scripts.lib();
 
   const end = () => {
     return l.get_log().map((s) => s.replaceAll(/^"|"$/g, '')).join("\n");
   }
 
-  //let outcome = args.t.call();
-  //$D(`Outcome: ${outcome}`);
+  let result = $fs.ast.t2_scraper({ t: args.t });
+  let locs = result;
 
-  let corp = args.t.name;
-  let { corps, corp_scripts } = query_db("f", undefined, { __corpdata: true })[0];
-  let ikeys = [].concat(...corps.map((c: string) => corp_scripts.map((s: string) => `${c}.${s}`)));
-  let require_confirm = false;
+  let locStrings = [];
 
-  // Check if the scriptor comes from a known npc corp
-  if (!ikeys.includes(corp)) {
-
-  }
-
-  if (require_confirm && !args.confirm) {
-    l.log("`4[WARN]` - Passed scriptor does not match any known npc corps")
-    l.log("`4[WARN]` - Cannot proceed with scan without confirmation")
-    l.log("`4[WARN]` - Call again with `Nconfirm`: `3true` to confirm")
-    return { ok: false, msg: end() }
-  }
-
-  l.log("`3[ OK ]` - Scriptor is known npc corp....")
-  let listicleRegex = /([a-z0-9]+\:(?:"?)[a-z0-9]+(?:"?))/;
-  let output = args.t.call({}) as string;
-  let listicles = listicleRegex.exec(output);
-  let retries = 0;
-
-  while (!listicles && retries < 4) {
-    listicles = listicleRegex.exec(output);
-    if (!listicles) {
-      l.log("`4[WARN]` - Scriptor returned unparsable output " + (retries + 1) + " times sequentially")
-      retries += 1;
+  for (let i = 0; i < locs.length; i++) {
+    let name = locs[i];
+    let [prefix, datablock, ...suffix] = name.split("_");
+    let loc_rank = 'Junkrack';
+    let loc_class = 'Architect';
+    let rank_str = datablock.substring(0, 2);
+    let class_str = datablock.substring(2, 5);
+    switch (rank_str) {
+      case 'jr': loc_rank = 'junkrack'; break;
+      case 'dd': loc_rank = 'diggerdeck'; break;
+      case 'wb': loc_rank = 'wreckbox'; break;
+      case 'pr': loc_rank = 'phreakrig'; break;
+      case 'ls': loc_rank = 'leetstack'; break;
     }
-    if (retries === 4) {
-      l.log("`D[ERR ]` - Cannot proceed, script output did not provide a result five times in a row");
-      l.log("`D[ERR ]` - The script may be corrupted in a way which makes ")
-      return { ok: false, msg: end() }
+
+
+    switch (class_str) {
+      case 'wvr': loc_class = 'architect'; break;
+      case 'ttl': loc_class = 'lock'; break;
+      case 'wlf': loc_class = 'infiltrator'; break;
+      case 'rvn': loc_class = 'scavenger'; break;
+      case 'stg': loc_class = 'executive'; break;
     }
+
+    let valid = true;
+    if (args.rank) valid = valid && loc_rank === args.rank;
+    if (args.class) valid = valid && loc_class === args.class;
+
+    if (!valid) continue;
+
+    locStrings.push(`${name}`)
   }
-  let [argument, page] = listicles[0].split(":");
 
 
-  l.log(`\`Y[DEBG]\` - page argument: \`N${argument}\``);
-  l.log(`\`Y[DEBG]\` - project list: \`3${page}\``);
-  return {
-    ok: true, msg: end()
-  };
+  let text = l.columnize(locStrings);
+  $D(text);
+
+  // //let outcome = args.t.call();
+  // //$D(`Outcome: ${outcome}`);
+
+  // let corp = args.t.name;
+  // let { corps, corp_scripts } = query_db("f", undefined, { __corpdata: true })[0];
+  // let ikeys = [].concat(...corps.map((c: string) => corp_scripts.map((s: string) => `${c}.${s}`)));
+  // let require_confirm = false;
+
+  // // Check if the scriptor comes from a known npc corp
+  // if (!ikeys.includes(corp)) {
+
+  // }
+
+  // if (require_confirm && !args.confirm) {
+  //   l.log("`4[WARN]` - Passed scriptor does not match any known npc corps")
+  //   l.log("`4[WARN]` - Cannot proceed with scan without confirmation")
+  //   l.log("`4[WARN]` - Call again with `Nconfirm`: `3true` to confirm")
+  //   return { ok: false, msg: end() }
+  // }
+
+  // l.log("`3[ OK ]` - Scriptor is known npc corp....")
+  // let listicleRegex = /([a-z0-9]+\:(?:"?)[a-z0-9]+(?:"?))/;
+  // let output = args.t.call({}) as string;
+  // let listicles = listicleRegex.exec(output);
+  // let retries = 0;
+
+  // while (!listicles && retries < 4) {
+  //   listicles = listicleRegex.exec(output);
+  //   if (!listicles) {
+  //     l.log("`4[WARN]` - Scriptor returned unparsable output " + (retries + 1) + " times sequentially")
+  //     retries += 1;
+  //   }
+  //   if (retries === 4) {
+  //     l.log("`D[ERR ]` - Cannot proceed, script output did not provide a result five times in a row");
+  //     l.log("`D[ERR ]` - The script may be corrupted in a way which makes ")
+  //     return { ok: false, msg: end() }
+  //   }
+  // }
+  // let [argument, page] = listicles[0].split(":");
+
+
+  // l.log(`\`Y[DEBG]\` - page argument: \`N${argument}\``);
+  // l.log(`\`Y[DEBG]\` - project list: \`3${page}\``);
+  // return {
+  //   ok: true, msg: end()
+  // };
 }
 
 
